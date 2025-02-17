b8223f5b2f264107d62465f438373995fa1f6f57
Paulinho <73448096+N7z@users.noreply.github.com>
2025-02-16 23:37:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import instaloader
 import os
 
+# Also before running main you should run save_session.py to generate your instagram session
+# Obligatory use of firefox web browser, if you don't use firefox change alter the code to your liking
+# But after generating the session just paste here in ./ folder, final name is going to be something like
+# session-username
+
+# Not the target for scraping, actually your instagram
 instagram_username = "xxxxxxxxx"
 
 def main(): 
@@ -21,7 +27,7 @@ def main():
     base_dir = f"{username}"
     os.makedirs(f"{base_dir}", exist_ok=True)
 
-    # Login no Instagram
+    # Try to login to instagram
     L = instaloader.Instaloader()
     print("\x1b[32m", end="")
     try:
@@ -33,11 +39,13 @@ def main():
     print("\x1b[35m[*] Searching profile...")
     profile = instaloader.Profile.from_username(L.context, username)
 
+    # Save followers
     print("\x1b[34m[*] Fetching followers...\x1b[0m")
     with open(f"{base_dir}/followers.txt", "w") as file:
         for follower in profile.get_followers():
             file.write(follower.username + "\n")
 
+    # Save followings
     print("\x1b[34m[*] Fetching followings...\x1b[0m")
     with open(f"{base_dir}/following.txt", "w") as file:
         for following in profile.get_followees():
